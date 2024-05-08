78a5d383b22abb5786ab3a41f0b84fc7624738e8
LanzeG <119438401+LanzeG@users.noreply.github.com>
2024-05-09 00:58:53 +0800

    haha

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
 // window.addEventListener('resize', checkDevTools);
 
 
-const values = ['RPA', 'Frontend', 'UI/UX', 'Lanze'];
+const values = ['Lanze', 'Frontend', 'UI/UX', 'RPA'];
 let index = 0;
 const textElement = document.querySelector('.animated-text');
 
