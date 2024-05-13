e546b6db8989de341b9072b4a7bcce5afe94c451
LanzeG <bermas.lester10@gmail.com>
2024-05-13 10:56:27 +0800

    gg

--- api/saveKey.js
+++ api/saveKey.js
@@ -40,7 +40,7 @@ async function updateVisitCount(generatedKey) {
       visitors.push(generatedKey);
 
       // Construct the data with the updated array of visitors
-      const data = { visitors };
+      const data = { visitors: [...visitors] };
 
       // Fetch options for the PUT request
       const putOptions = {
