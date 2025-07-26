b4c1c5880de1414ee622120b673f6e35a03e707a
LanzeG <bermas.lester10@gmail.com>
2025-07-26 18:16:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -298,9 +298,9 @@ function embedVismeForm() {
   var systemCount = document.getElementById('system-count');
   var languageCount = document.getElementById('language-count');
 
-  animateValue(customerCount, 0, 7, 4000); 
-  animateValue(systemCount, 0, 24, 4000); 
-  animateValue(languageCount, 0, 11, 4000);
+  animateValue(customerCount, 0, 2, 4000); 
+  animateValue(systemCount, 0, 32, 4000); 
+  animateValue(languageCount, 0, 21, 4000);
 
 
   function generateUniqueKey() {
